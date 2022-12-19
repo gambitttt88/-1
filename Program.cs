4ab7eac254f64214cc29be4fd9abbc8ad2ca6f86
Sergey <gambitttt@mail.ru>
2022-12-19 22:05:07 +0300

    дз

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿/*Задача 1
+Console.WriteLine("Введите число a: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int number1 = int.Parse(Console.ReadLine());
+
+if(number > number1)
+{
+   Console.WriteLine($"Число {number} - max, число {number1} - min"); 
+}
+else
+{
+ Console.WriteLine($"Число {number1} - max, число {number} - min");   
+}*/
+
+/*Задача 2
+Console.WriteLine("Введите число a: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число с: ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number;
+if(max < number1) 
+max = number1;
+if(max < number2)
+max = number2;
+
+{
+   Console.WriteLine($"Число {max} - max"); 
+}*/
+
+/*Задача 3
+Console.WriteLine("Введите число любое число: ");
+int number = int.Parse(Console.ReadLine());
+if(number%2==0)
+{
+   Console.WriteLine($"Число {number} - четное"); 
+}
+else
+{
+   Console.WriteLine($"Число {number} - НЕ четное"); 
+}*/
+
+/*Задача 4
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int N1 = 1;
+
+while (N1 <= N)
+{
+   if(N1%2==0)
+{Console.Write($"{N1} ");
+ }
+ 
+ N1 = N1+1;
+}*/
